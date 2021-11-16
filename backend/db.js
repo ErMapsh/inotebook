405b8daa8eb23da3f0bfc5f0050fe27ea04640f1
Mahesh Mestri <72149385+ErMapsh@users.noreply.github.com>
2021-11-16 15:07:15 +0530

    inotebook db connecting and express server setuping

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const mongoURL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+//connecting to mongo
+const connectToMongo = async ()=>{
+    mongoose.connect(mongoURL, ()=>{
+        console.log("connected to Mongo Successfully");
+    })
+}
+
+// want to export something
+module.exports = connectToMongo;

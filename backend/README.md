4eb163ff34f542a6c484b4de376796167af8c522
Mahesh Mestri <72149385+ErMapsh@users.noreply.github.com>
2021-11-18 10:12:06 +0530

    use bcryptsjs for encrypt password to store in database and jwt

--- backend/README.md
+++ backend/README.md
@@ -4,3 +4,10 @@
 - Storing Data in database using moongose model
 - we want to validate body info so we express validator (npm install --save express-validator)
 - refactor auth.js,in these we if user email is already register then is show email id is already     register, so user cant register with that email id. we alos make a collection for endpoints is easy while developing 
+
+## using bcryptsjs module making site secure , JWT (json web token)
+- npm install bcryptjs  
+- bcrypts module having function like genSaltSync for generating salt value and for hashing hash function
+- and this hash value will be passed to password of user while creating new user.
+- jwt is nothing but after login automatically login that account, that we login in previously. that all things in json web token(npm i jsonwebtoken)
+
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const router = express.Router(); //need to export
-const User = require("../models/User");
-const { body, validationResult } = require("express-validator");
+const express = require("express"); //express for routing
+const router = express.Router(); //router is for routing and router is in express
+const User = require("../models/User"); //need a model of user to create user
+const { body, validationResult } = require("express-validator"); //express-validater to validate body info is in specific format like name length.
+const bcrypt = require("bcryptjs"); //hashing alogirithm (password+salt)
+var jwt = require('jsonwebtoken');//for authentication
+
+const JWT_SECRET_KEY = "ermapshisagoodb%oy$ermapshisagoodb%oy$"
 
 //create a User using: Post "/api/auth/createuser". Doesn't require login
 router.post(
@@ -12,7 +16,9 @@ router.post(
     body("password", "Password must be at least ").isLength({ min: 8 }),
   ],
 
+  //function has begin
   async (req, res) => {
+
     // if there are error, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -21,26 +27,37 @@ router.post(
     }
 
     try {
-      //check whether the user with this email exists already
-      let user = await User.findOne({ email: req.body.email });
-      // console.log(user);//this show null if user email not in database
       
+      let user = await User.findOne({ email: req.body.email });//check whether the user with this email exists already
+      // console.log(user);//this show null if user email not in database
+
       if (user) {
         return res
           .status(400)
           .json({ error: "Sorry, User already exist with this email " });
       }
 
-      //getting inforation from body
-      await User.create({
+      const salt = await bcrypt.genSaltSync(10); //generating salt
+      const securePass = await bcrypt.hash(req.body.password, salt); //use hash function
+
+      //creating a new user
+      userinfo = await User.create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: securePass,
       });
-      res.json("submit succesfully");
+      
+
+      //JWT for after login we dont need to login everytime except logout
+      const data = {user: {id: userinfo.id}};
+      const authtoken = await jwt.sign(data, JWT_SECRET_KEY);
+      console.log(authtoken);
+      res.json({"this authtoken is generated first time while registering ": authtoken})
+      // res.json("submit succesfully");// res.json(userinfo);
+
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("some error occur")
+      res.status(500).send("some error occur");
     }
   }
 );

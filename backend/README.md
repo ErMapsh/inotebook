58e3d1342913cec834fac7ce97cfc4898ec51e56
Mahesh Mestri <72149385+ErMapsh@users.noreply.github.com>
2021-11-18 13:16:10 +0530

    creating login endpoint and send jwt

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,7 @@
+## in js , lots function reuturn promisesn so we need use await in async function
+- if u not use await then some times error occur
+- in my way await is promise, need to wait until he do not return his promise.
+
 ## this is backend of aur application
 - we creating spearate backend folder for aur backend things.
 - How can we creating routes for endpoints and schema model for Notes and Users.
@@ -7,7 +11,6 @@
 
 ## using bcryptsjs module making site secure , JWT (json web token)
 - npm install bcryptjs  
-- bcrypts module having function like genSaltSync for generating salt value and for hashing hash function
+- #bcrypts module: having function like genSaltSync for generating salt value and for hashing hash function
 - and this hash value will be passed to password of user while creating new user.
 - jwt is nothing but after login automatically login that account, that we login in previously. that all things in json web token(npm i jsonwebtoken)
-
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,10 @@ const router = express.Router(); //router is for routing and router is in expres
 const User = require("../models/User"); //need a model of user to create user
 const { body, validationResult } = require("express-validator"); //express-validater to validate body info is in specific format like name length.
 const bcrypt = require("bcryptjs"); //hashing alogirithm (password+salt)
-var jwt = require('jsonwebtoken');//for authentication
+var jwt = require("jsonwebtoken"); //for authentication
+const JWT_SECRET_KEY = "ermapshisagoodb%oy$ermapshisagoodb%oy$"; //secret key
 
-const JWT_SECRET_KEY = "ermapshisagoodb%oy$ermapshisagoodb%oy$"
-
-//create a User using: Post "/api/auth/createuser". Doesn't require login
+//Route1: create a User using: Post "/api/auth/createuser". Doesn't require login
 router.post(
   "/createuser",
   [
@@ -16,19 +15,16 @@ router.post(
     body("password", "Password must be at least ").isLength({ min: 8 }),
   ],
 
-  //function has begin
   async (req, res) => {
-
     // if there are error, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log();
+      // console.log(error);
       return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-      
-      let user = await User.findOne({ email: req.body.email });//check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email }); //check whether the user with this email exists already
       // console.log(user);//this show null if user email not in database
 
       if (user) {
@@ -46,20 +42,81 @@ router.post(
         email: req.body.email,
         password: securePass,
       });
-      
 
       //JWT for after login we dont need to login everytime except logout
-      const data = {user: {id: userinfo.id}};
-      const authtoken = await jwt.sign(data, JWT_SECRET_KEY);
-      console.log(authtoken);
-      res.json({"this authtoken is generated first time while registering ": authtoken})
+      const payload = { user: { id: userinfo.id } };
+      const authtoken = await jwt.sign(payload, JWT_SECRET_KEY);
+      // console.log(authtoken);
+      res.json({
+        "this authtoken is generated first time while registering ": authtoken,
+      });
       // res.json("submit succesfully");// res.json(userinfo);
-
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("some error occur");
+      res.status(500).send("Internal Server Error Occur");
     }
   }
 );
 
+//Route2: Authenticate user after creating account, "POST": "/api/auth/login". Doesn't require login
+router.post(
+  "/loginuser",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blacnk ").exists(),
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req); // if there are error, return Bad request and the errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body; //getting  information of user from document.body
+    try {
+      let userinfo = await User.findOne({ email }); //finding user already register with this email or not
+      if (!userinfo) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" }); //if user not register then this response will see
+      } else {
+        // Comparing password means our password is c
+        const passwordCompare = await bcrypt.compare(
+          password,
+          userinfo.password
+        );
+        // console.log(passwordCompare)
+        if (!passwordCompare) {
+          return res
+            .status(400)
+            .json({ error: "Please try to login with correct credentials" });
+        }
+
+        const payload = { user: { id: userinfo.id } };
+        const authtoken = await jwt.sign(payload, JWT_SECRET_KEY);
+        res.json({
+          "this authtoken is generated first time while registering ":
+            authtoken,
+        });
+      }
+
+      console.log("login succesfull");
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error Occur");
+    }
+  }
+);
+
+// Route3: Get uesrdetails user details using : POST '/api/auth/getuser. login required
 module.exports = router;
+router.post(
+  "/getuser",
+  // try{
+
+  // }catch{
+  //   console.log(error.message);
+  //   res.status(500).send("Internal Server Error Occur");
+  //   }
+  // }
+)

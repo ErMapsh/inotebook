b0ea457eb0d0c2cfd593c0c65f09dbe0582b9123
Mahesh Mestri <72149385+ErMapsh@users.noreply.github.com>
2021-11-25 10:14:53 +0530

    unsterand what is context api and update/write readme

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Alert() {
     return (
         <div style={{height: "50px", marginTop: "50px"}}>
-            <div className="alert alert-success text-center" role="alert">
+            {/* <div className="alert alert-success text-center" role="alert">
                 A simple success alert—check it out!
-            </div>
+            </div> */}
         </div>
     )
 }
